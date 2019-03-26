4b5b4d9c56dedc02d17e2428e94ac52c955a168e
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-03-26 17:07:56 -0400

    Add .bashrc to terminal-config

--- windows-ubuntu.sh
+++ windows-ubuntu.sh
@@ -1,4 +1,5 @@
 ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.bash_profile ~
+ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.bashrc ~
 ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.gitconfig ~
 ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.gitignore ~
 ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.vimrc ~

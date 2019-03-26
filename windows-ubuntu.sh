f85805442dcf58a95f72985739c66b1b5f9eacee
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-03-26 14:07:40 -0400

    Add configuration script for windows-bash

---
+++ windows-ubuntu.sh
@@ -0,0 +1,5 @@
+ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.bash_profile ~
+ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.gitconfig ~
+ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.gitignore ~
+ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.vimrc ~
+

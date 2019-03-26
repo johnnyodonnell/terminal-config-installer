ed3a2fed9140e49d7c0ad8b4605f10e616cb6942
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-03-26 14:34:16 -0400

    Add bin folder to terminal config for windows ubuntu

--- windows-ubuntu.sh
+++ windows-ubuntu.sh
@@ -2,4 +2,6 @@ ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.bash_profile ~
 ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.gitconfig ~
 ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.gitignore ~
 ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.vimrc ~
+ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.vimrc ~
+ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/bin ~
 

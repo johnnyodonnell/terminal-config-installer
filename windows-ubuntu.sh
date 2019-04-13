f9de1ad2d01d663f6df7ba880020e3c76b2a463b
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-04-13 11:14:39 -0400

    Make Window username an input of the script

--- windows-ubuntu.sh
+++ windows-ubuntu.sh
@@ -1,8 +1,11 @@
-ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.bash_profile ~
-ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.bashrc ~
-ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.gitconfig ~
-ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.gitignore ~
-ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.vimrc ~
-ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/.vimrc ~
-ln -s /mnt/c/Users/Johnny/Dropbox/terminal-config/bin ~
+echo What is your Windows username?
+read winuser
+
+ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.bash_profile ~
+ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.bashrc ~
+ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.gitconfig ~
+ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.gitignore ~
+ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.vimrc ~
+ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.vimrc ~
+ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/bin ~
 

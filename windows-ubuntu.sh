c248f4b4328a9afe684616fcad17429c58867c73
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-08-26 17:22:40 -0700

    In the wsl ubuntu script, remove any existing files first before making a symlink

--- windows-ubuntu.sh
+++ windows-ubuntu.sh
@@ -1,11 +1,17 @@
 echo What is your Windows username?
 read winuser
 
+rm -f ~/.bash_profile
+rm -f ~/.bashrc
+rm -f ~/.gitconfig
+rm -f ~/.gitignore
+rm -f ~/.vimrc
+rm -rf ~/bin
+
 ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.bash_profile ~
 ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.bashrc ~
 ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.gitconfig ~
 ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.gitignore ~
-ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.vimrc ~
 ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/.vimrc ~
 ln -s /mnt/c/Users/$winuser/Dropbox/terminal-config/bin ~
 

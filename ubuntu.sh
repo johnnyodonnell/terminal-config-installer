df6c0b0b74092b26fb8d8a1448b0f71fbf982940
Johnny O'Donnell <odjohnny@amazon.com>
2019-11-15 10:14:02 -0800

    Add unison preferences to ubuntu installer script

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,12 +3,15 @@
 echo What is your username?
 read username
 
+mkdir -p ~/.unison
+
 rm -f ~/.bash_profile
 rm -f ~/.bashrc
 rm -f ~/.gitconfig
 rm -f ~/.gitignore
 rm -f ~/.vimrc
 rm -rf ~/bin
+rm -f ~/.unison/*
 
 ln -s /home/$username/Dropbox/terminal-config/.bash_profile ~
 ln -s /home/$username/Dropbox/terminal-config/.bashrc ~
@@ -16,4 +19,5 @@ ln -s /home/$username/Dropbox/terminal-config/.gitconfig ~
 ln -s /home/$username/Dropbox/terminal-config/.gitignore ~
 ln -s /home/$username/Dropbox/terminal-config/.vimrc ~
 ln -s /home/$username/Dropbox/terminal-config/bin ~
+ln -s /home/$username/Dropbox/terminal-config/version-sets.prf ~/.unison/
 
